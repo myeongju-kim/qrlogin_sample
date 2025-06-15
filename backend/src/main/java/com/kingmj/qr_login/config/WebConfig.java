33a83402f6eecb034ea6600b82a2c21c813d6af7
myeongju-kim <mjoo1106@naver.com>
2025-06-15 23:15:10 +0900

    feat: webConfig CORS 설정

---
+++ backend/src/main/java/com/kingmj/qr_login/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.kingmj.qr_login.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private static final String DEFAULT_VUE_URL = "http://localhost:5173";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(DEFAULT_VUE_URL)
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
