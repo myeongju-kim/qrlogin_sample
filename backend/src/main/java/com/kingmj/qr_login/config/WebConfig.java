f6debdc3cbb3a9360d43f11dd445315bff5ed356
myeongju-kim <mjoo1106@naver.com>
2025-06-17 00:18:18 +0900

    feat: localhost 네트워크용 도메인 추가

--- backend/src/main/java/com/kingmj/qr_login/config/WebConfig.java
+++ backend/src/main/java/com/kingmj/qr_login/config/WebConfig.java
@@ -6,12 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private static final String DEFAULT_VUE_URL = "http://localhost:5173";
+    private static final String[] DEFAULT_VUE_URLS = {"http://localhost:5173", "http://172.30.1.65:5173"};
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(DEFAULT_VUE_URL)
+                .allowedOrigins(DEFAULT_VUE_URLS)
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
